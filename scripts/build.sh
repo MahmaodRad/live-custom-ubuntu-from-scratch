ec6260a2405a85cc6505257f6d4f8de3b388511e
MahmaodRad <98706620+MahmaodRad@users.noreply.github.com>
2025-09-01 10:56:50 +0000

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -55,8 +55,9 @@ function chroot_exit_teardown() {
     sudo chroot chroot umount /proc
     sudo chroot chroot umount /sys
     sudo chroot chroot umount /dev/pts
-    sudo umount chroot/dev
-    sudo umount chroot/run
+    sudo umount -f  chroot/dev
+    sleep 100
+    sudo umount -f chroot/run
 }
 
 function check_host() {
